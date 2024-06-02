26249f24e929f14e7b75b0097c986d46ebab2c1f
Miguel Rodrigues <miguel.rodrigues8@hotmail.com>
2024-06-02 21:54:17 +0100

    fix: env variables usage

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -1,9 +1,11 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
+  const url =  process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    url!,
+    key!
   );
 }
--- src/app/utils/supabase/middleware.ts
+++ src/app/utils/supabase/middleware.ts
@@ -1,5 +1,5 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
   let response = NextResponse.next({
@@ -8,9 +8,12 @@ export async function updateSession(request: NextRequest) {
     },
   })
 
+  const url =  process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    url!,
+    key!,
     {
       cookies: {
         get(name: string) {
