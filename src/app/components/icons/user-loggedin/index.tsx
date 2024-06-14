1b924459d8b19ead65c43d71858748575efce5a4
Miguel Rodrigues <miguel.rodrigues8@hotmail.com>
2024-06-14 20:50:17 +0100

    fix: icon props to follow react props

--- src/app/components/icons/user-loggedin/index.tsx
+++ src/app/components/icons/user-loggedin/index.tsx
@@ -7,9 +7,9 @@ const UserLoggedIn = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-user-round-check"
     >
       <path d="M2 21a8 8 0 0 1 13.292-6" />
